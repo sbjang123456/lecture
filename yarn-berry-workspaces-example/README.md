7675fce247d03b4728abc322744f06a1f54fa9f1
sbjang <sbjang123456@gmail.com>
2023-12-13 20:27:17 +0900

    yarn-berry-workspaces

---
+++ yarn-berry-workspaces-example/README.md
@@ -0,0 +1,3 @@
+# yarn-berry-workspaces-example
+
+- [Guide](https://2woongjae.notion.site/yarn-berry-workspaces-example-b9d4d5c47f7d46cfbbb8e0bbcd7260b7)
---
+++ yarn-berry-workspaces-example/packages/b/index.js
@@ -0,0 +1,9 @@
+// yarn-berry-workspaces-example/packages/b/index.js
+
+const axios = require("axios");
+
+module.exports = async function () {
+  const response = await axios.get("https://api.github.com/users");
+
+  return response.data;
+};
