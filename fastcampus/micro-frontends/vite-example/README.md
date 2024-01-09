ee5f35219670d45447d9126d24090e6779f58494
sbjang <sbjang123456@gmail.com>
2024-01-10 07:40:03 +0900

    [#3] vite 개발 툴 예제 소스 작성

---
+++ fastcampus/micro-frontends/vite-example/README.md
@@ -0,0 +1,3 @@
+# vite-example
+
+- [Guide](https://2woongjae.notion.site/vite-example-d7d68dc491eb4533a9eb7e073ab92ffd)
---
+++ fastcampus/micro-frontends/vite-example/packages/ui-kit/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ insertTypesEntry: true })],
+  build: {
+    lib: {
+      entry: "src/lib.ts",
+      name: "ui-kit",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
