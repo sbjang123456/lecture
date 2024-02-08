b4d2d9da9ca3675b9afc8d4adc8504af04648b21
sbjang <sbjang123456@gmail.com>
2024-02-08 13:56:09 +0900

    [#4] mfa nginx proxy 통합 예제

---
+++ fastcampus/micro-frontends/micro-frontends-with-proxy/README.md
@@ -0,0 +1,3 @@
+# micro-frontends-with-proxy
+
+- [Guide](https://2woongjae.notion.site/Nginx-proxy-56bf75a7d405442b891b1d9083ee0f92)
---
+++ fastcampus/micro-frontends/micro-frontends-with-proxy/teams/team-home/src/main.ts
@@ -0,0 +1,14 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>Home</h1>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/jobs/">Jobs</a></li>
+        <li><a href="/network/">Network</a></li>
+      </ul>
+    </nav>
+  </div>
+`;
