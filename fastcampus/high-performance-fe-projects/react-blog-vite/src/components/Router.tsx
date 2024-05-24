e605e66184a8fff3f93fb4c0150e24ecfb890cdc
sbjang <sbjang123456@gmail.com>
2024-05-24 15:14:51 +0900

    chore: [#7] tsconfig paths 설정

--- fastcampus/high-performance-fe-projects/react-blog-vite/src/components/Router.tsx
+++ fastcampus/high-performance-fe-projects/react-blog-vite/src/components/Router.tsx
@@ -1,12 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "../pages/home";
-import PostList from "../pages/posts";
-import PostDetail from "../pages/posts/detail";
-import PostNew from "../pages/posts/new";
-import PostEdit from "../pages/posts/edit";
-import Profile from "../pages/profile";
-import LoginPage from "../pages/login";
-import SignUpPage from "../pages/signup";
+import Home from "pages/home";
+import PostList from "pages/posts";
+import PostDetail from "pages/posts/detail";
+import PostNew from "pages/posts/new";
+import PostEdit from "pages/posts/edit";
+import Profile from "pages/profile";
+import LoginPage from "pages/login";
+import SignUpPage from "pages/signup";
 
 export default function Router() {
   return (
