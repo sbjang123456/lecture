383b29cb266f06b9abcbecb1948deb59f522db51
sbjang <sbjang123456@gmail.com>
2024-05-24 13:34:07 +0900

    feat: [#7] 컴포넌트 만들기 > 홈 컴포넌트 작업

--- fastcampus/high-performance-fe-projects/react-blog-vite/src/pages/home/index.tsx
+++ fastcampus/high-performance-fe-projects/react-blog-vite/src/pages/home/index.tsx
@@ -10,11 +10,33 @@ export default function Home() {
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-      <div className="post-list">Post List</div>
+      <div className="post__navigation">
+        <div className="post__navigation--active">전체</div>
+        <div>나의 글</div>
+      </div>
+      <div className="post__list">
+        {[...Array(10)].map((_, i) => (
+          <div key={i} className="post__box">
+            <Link to={`/posts/${i}`}>
+              <div className="post__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">author</div>
+                <div className="post__date">date</div>
+              </div>
+              <div className="post__title">title</div>
+              <div className="post__text">contents</div>
+              <div className="post__utils-box">
+                <div className="post__delete">삭제</div>
+                <div className="post__edit">수정</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <div>m1</div>
-        <div>m1</div>
-        <div>m1</div>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
       </footer>
     </div>
   );
