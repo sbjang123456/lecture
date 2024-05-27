c7fb2db296e33a3ec5395c5f08d38048a0efd5d2
sbjang <sbjang123456@gmail.com>
2024-05-27 10:22:11 +0900

    feat: [#7] 사용자 인증 구현 > firebase 설정

--- fastcampus/high-performance-fe-projects/react-blog-vite/src/main.tsx
+++ fastcampus/high-performance-fe-projects/react-blog-vite/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import firebase from "firebaseApp";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
