878312fec4fe680645accfeed41f4c7de204c930
sbjang <sbjang123456@gmail.com>
2024-05-28 14:46:55 +0900

    feat: 다크모드 구현

--- fastcampus/high-performance-fe-projects/react-blog-vite/src/App.tsx
+++ fastcampus/high-performance-fe-projects/react-blog-vite/src/App.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { app } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Router from "./components/Router";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Loader from "components/Loader";
+import ThemeContext from "context/ThemeContext";
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
   // auth 를 체크하기 전 loader 를 띄워주는 용도
   const [init, setInit] = useState(false);
@@ -23,11 +25,12 @@ function App() {
       setInit(true);
     });
   }, [auth]);
+
   return (
-    <>
+    <div className={context.theme === "light" ? "white" : "dark"}>
       <ToastContainer />
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
-    </>
+    </div>
   );
 }
 
--- fastcampus/high-performance-fe-projects/react-blog-vite/src/main.tsx
+++ fastcampus/high-performance-fe-projects/react-blog-vite/src/main.tsx
@@ -3,11 +3,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthContextProvider } from "context/AuthContext.tsx";
+import { ThemeContextProvider } from "context/ThemeContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <AuthContextProvider>
-    <Router>
-      <App />
-    </Router>
-  </AuthContextProvider>
+  <ThemeContextProvider>
+    <AuthContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthContextProvider>
+  </ThemeContextProvider>
 );
