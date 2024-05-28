88a71bcda9bbb4e2f6fc18820c961ac09c0ce21e
sbjang <sbjang123456@gmail.com>
2024-05-29 08:30:00 +0900

    feat: [#7] 댓글 > 댓글 등록 구현

--- fastcampus/high-performance-fe-projects/react-blog-vite/src/components/Comments.tsx
+++ fastcampus/high-performance-fe-projects/react-blog-vite/src/components/Comments.tsx
@@ -1,11 +1,21 @@
-import { ChangeEvent, useState } from "react";
+import { PostProps } from "components/PostList";
+import AuthContext from "context/AuthContext";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import { ChangeEvent, FormEvent, useContext, useState } from "react";
+import { toast } from "react-toastify";
 
 const COMMENTS = [
   { id: 1, email: "test@", content: "comment1", createdAt: "2021-10-10" },
 ];
 
-export default function Comments() {
+interface CommentsProps {
+  post: PostProps;
+}
+
+export default function Comments({ post }: CommentsProps) {
   const [comment, setComment] = useState("");
+  const { user } = useContext(AuthContext);
 
   const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setComment(e.target.value);
@@ -17,9 +27,47 @@ export default function Comments() {
       setComment(value);
     }
   };
+
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      if (post && post?.id) {
+        const postRef = doc(db, "posts", post.id);
+
+        if (user?.uid) {
+          const commentObj = {
+            content: comment,
+            uid: user?.uid,
+            email: user?.email,
+            createdAt: new Date().toLocaleDateString("ko", {
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            }),
+          };
+
+          await updateDoc(postRef, {
+            comments: arrayUnion(commentObj),
+            updatedAt: new Date().toLocaleDateString("ko", {
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            }),
+          });
+
+          toast.success("댓글이 작성되었습니다.");
+          setComment("");
+        }
+      }
+    } catch (e: any) {
+      console.error(e);
+      toast.error(e?.code);
+    }
+  };
   return (
     <div className="comments">
-      <form action="" className="comments__form">
+      <form className="comments__form" onSubmit={onSubmit}>
         <div className="form__block">
           <label htmlFor="comment">댓글 입력</label>
           <textarea
--- fastcampus/high-performance-fe-projects/react-blog-vite/src/components/PostDetail.tsx
+++ fastcampus/high-performance-fe-projects/react-blog-vite/src/components/PostDetail.tsx
@@ -68,7 +68,7 @@ export default function PostDetail() {
               {post.content}
             </div>
           </div>
-          <Comments />
+          <Comments post={post} />
         </>
       ) : (
         <Loader />
