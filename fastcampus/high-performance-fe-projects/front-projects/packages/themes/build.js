a6b88f6ad44109f6f86438845f0fdfade9cd11d3
sbjang <sbjang123456@gmail.com>
2024-06-05 14:45:32 +0900

    feat: [#14] 프로젝트 생성 및 환경설정 - 공통 config 설정 (esbuild-config, lint, prettier...)

--- fastcampus/high-performance-fe-projects/front-projects/packages/themes/build.js
+++ fastcampus/high-performance-fe-projects/front-projects/packages/themes/build.js
@@ -1,41 +1,6 @@
-import esbuild from "esbuild";
+import run from '@sbjang/esbuild-config';
 import pkg from "./package.json" assert { type: "json" };
 
-const dev = process.argv.includes("--dev");
-const minify = !dev;
-
-const watch = process.argv.includes("--watch");
-
-const external = Object.keys({
-  ...pkg.devDependencies,
-  ...pkg.dependencies,
-  ...pkg.peerDependencies,
-});
-
-const baseConfig = {
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  minify,
-  sourcemap: true,
-  outdir: "dist",
-  target: "es2019",
-  watch,
-  external,
-};
-
-Promise.all([
-  esbuild.build({
-    ...baseConfig,
-    format: "esm",
-  }),
-  esbuild.build({
-    ...baseConfig,
-    format: "cjs",
-    outExtension: {
-      ".js": ".cjs",
-    },
-  }),
-]).catch(() => {
-  console.error("Build Failed");
-  process.exit(1);
-});
+run({
+  pkg,
+})
